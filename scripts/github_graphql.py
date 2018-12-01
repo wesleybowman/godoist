b68a6e42f0a22a706251401f3ca630b8ec8ad660
wesleybowman <wesley.bowman23@gmail.com>
2018-12-01 15:26:14 +0100

    Change example to be more relevant
    Still not where I want it to be, but this is a start.
    
    I also added __pycache__ to the gitignore. Next will be an example of the
    config file.

--- scripts/github_graphql.py
+++ scripts/github_graphql.py
@@ -1,40 +1,46 @@
 """
-An example gotten from:
-https://gist.github.com/gbaman/b3137e18c739e0cf98539bf4ec4366ad
-
-I plan to modify to get a test going
+An example of how to use the GitHub GraphQL API
 """
-# An example to get the remaining rate limit using the Github GraphQL API.
-
 import requests
 
-# TODO: Add a config file that holds the API keys for now
-headers = {"Authorization": "Bearer YOUR API KEY"}
+from config import config
+
+HEADERS = {"Authorization": f"Bearer {config['GITHUB_PERSONAL_ACCESS_TOKEN']}"}
 
 
-def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
-    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+def run_query(query):
+    """
+    A simple function to use requests.post to make the API call. Note the json= section.
+    """
+    request = requests.post('https://api.github.com/graphql',
+                            json={'query': query},
+                            headers=HEADERS)
+
     if request.status_code == 200:
         return request.json()
+
     else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+        raise Exception(f'Query failed to run by returning code of {request.status_code}. {query}')
 
         
 # The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.       
 query = """
-{
-  viewer {
-    login
-  }
-  rateLimit {
-    limit
-    cost
-    remaining
-    resetAt
+query {
+  search(query:  "repo:wesleybowman/godoist author:wesleybowman", type: ISSUE, first: 10) {
+    repositoryCount
+    edges {
+      node {
+        ... on Issue {
+          bodyText
+          title
+          updatedAt
+        }
+      }
+    }
   }
 }
 """
 
-result = run_query(query) # Execute the query
-remaining_rate_limit = result["data"]["rateLimit"]["remaining"] # Drill down the dictionary
-print("Remaining rate limit - {}".format(remaining_rate_limit))
+# Execute the query
+result = run_query(query)
+print(f'Result: {result}')
