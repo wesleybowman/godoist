7830e820f670761899284c7c750ca0495c395ca5
wesleybowman <wesley.bowman23@gmail.com>
2018-12-08 15:28:45 +0100

    Add todoist api token in example config

--- config_example.py
+++ config_example.py
@@ -1,3 +1,4 @@
 config = {
-    'GITHUB_PERSONAL_ACCESS_TOKEN': '<your_personal_access_token_here>'
+    'GITHUB_PERSONAL_ACCESS_TOKEN': '<your_personal_access_token_here>',
+    'TODOIST_API_TOKEN': '<your_personal_api_token_here>',
 }
